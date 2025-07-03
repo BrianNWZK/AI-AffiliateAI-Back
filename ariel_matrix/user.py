52dc42e5a5ecce2a27f2c820ec665d16885048e7
BrianNWZK <brianwaezike@gmail.com>
2025-07-03 19:35:12 +0100

    Update user.py

--- ariel_matrix/user.py
+++ ariel_matrix/user.py
@@ -3,3 +3,6 @@ from ariel_matrix.ariel_user import ARIEL, ArielUser
 # For compatibility, expose SYSTEM_USER and ARIEL_MATRIX_USER as aliases
 SYSTEM_USER = ARIEL
 ARIEL_MATRIX_USER = ARIEL
+
+# Alias for compatibility with 'from ariel_matrix.user import User'
+User = ArielUser
